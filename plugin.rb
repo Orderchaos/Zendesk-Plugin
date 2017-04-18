3f7c0561ebb806fbc5c09af6d5b5cf7cdb177f83
Sean Gaulin <Orderchaos@users.noreply.github.com>
2017-04-17 22:03:46 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@ gem 'inflection', '1.0.0'
 gem 'zendesk_api', '1.14.0'
 
 
-register_asset "javascripts/topic_route_cont.js"
+#register_asset "javascripts/topic_route_cont.js"
 register_asset "javascripts/zendesk_button.js"
 register_asset "stylesheets/buttons_cont.css.scss"
 
